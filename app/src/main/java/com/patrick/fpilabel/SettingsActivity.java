e975c8257396bf3f6333be162251f8aedda150d5
Patrick Snelgar <patrick_snelgar@live.com>
2020-11-26 16:40:09 +1300

    Added option to select a file in the preferences, this will be used to read labels from. TODO:  - implement file reader in main activity to make use of the new Label list preference.  - resize text panel to accommodate longer strings (set reasonable limit).  - remove hardcoded default label

--- app/src/main/java/com/patrick/fpilabel/SettingsActivity.java
+++ app/src/main/java/com/patrick/fpilabel/SettingsActivity.java
@@ -1,10 +1,27 @@
 package com.patrick.fpilabel;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContentResolverCompat;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -22,10 +39,64 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+            SharedPreferences.Editor e = getPreferenceManager().getSharedPreferences().edit();
+            e.putString("label_file", "----");
+            e.commit();
+        }
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            Preference labelList = (Preference) findPreference("label_file");
+            labelList.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent i = new Intent().setType("text/plain").setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(i, 7);
+                    return true;
+                }
+            });
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            //Log.d("SettingsActivity", "Got result for request: " + requestCode + " with resultCode: " + resultCode);
+            if(requestCode == 7 && resultCode == RESULT_OK) {
+
+                Uri uri = data.getData();
+
+                SharedPreferences preferenceManager = getPreferenceManager().getSharedPreferences();
+                SharedPreferences.Editor editor = preferenceManager.edit();
+                Log.d("SettingsActivity", "onActivityResult: adding " + uri.getPath() + " to preferences");
+                editor.putString("label_file", uri.getPath());
+                editor.commit();
+            }
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+            SharedPreferences sp = getPreferenceManager().getSharedPreferences();
+            Log.i("onPreferenceChanged", "Preference with key: "+ s + " has changed");
+            switch (s){
+                case "label_file":
+                    Preference p = findPreference(s);
+                    String t[] = sp.getString("label_file", "").split("/");
+                    String fileName = "----";
+                    if(t.length > 1){
+                        fileName = t[t.length - 1];
+                    }
+                    p.setSummary(fileName);
+                    break;
+                default:
+            }
         }
     }
 }
